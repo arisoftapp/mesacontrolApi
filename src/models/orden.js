0de9b65b1be45dfa8281eab6a325bbae7cf91dd5
Frank Crow <frank@arisoft.com.mx>
2019-07-30 17:57:42 -0600

    Orden put

--- src/models/orden.js
+++ src/models/orden.js
@@ -4,7 +4,7 @@ let ordenModel = {};
 ordenModel.getOrdenes = (callback) => {
     //console.log(idEmpresa);
     if (dbAdmin) {
-        dbAdmin.query(`SELECT a.id_orden, CONCAT(a.benef_nombre, " ", a.benef_paterno, " ", a.benef_materno) AS nombre_beneficiario, CONCAT(b.nombre," ",b.ap_paterno," ", b.ap_materno) AS nombre_tecnico, c.nombre_aseguradora, d.orden_status AS estado_orden FROM orden AS a
+        dbAdmin.query(`SELECT a.id_orden, a.id_status, a.levantamiento, CONCAT(a.benef_nombre, " ", a.benef_paterno, " ", a.benef_materno) AS nombre_beneficiario, CONCAT(b.nombre," ",b.ap_paterno," ", b.ap_materno) AS nombre_tecnico, c.nombre_aseguradora, d.orden_status AS estado_orden FROM orden AS a
         INNER JOIN tecnico AS b ON a.id_tecnico = b.id_tecnico
         INNER JOIN aseguradora AS c ON a.id_aseguradora = c.id_aseguradora
         INNER JOIN estado_orden AS d ON a.id_status = d.id_status`, function(err, rows) {
@@ -51,34 +51,33 @@ ordenModel.insertOrden = (ordenData, callback) => {
 ordenModel.updateOrden = (ordenData, callback) =>{
     if (dbAdmin){
         const sql = `UPDATE orden SET 
-                nombre = ${ordenData.nombre},
-                ap_paterno = ${ordenData.ap_paterno},
-                ap_materno = ${ordenData.ap_materno},
-                RFC = ${ordenData.RFC},
-                cedula_prof = ${ordenData.cedula_prof},
-                universidad = ${ordenData.universidad},
-                especialidad_1 = ${ordenData.especialidad_1},
-                especialidad_2 = ${ordenData.especialidad_2},
-                tel_fijo = ${ordenData.tel_fijo},
-                tel_cel = ${ordenData.tel_cel},
-                tel_adicional = ${ordenData.tel_adicional},
-                email = ${ordenData.email},
-                id_consultorio = ${ordenData.id_consultorio},
-                calle = ${ordenData.calle},
-                no_ext = ${ordenData.no_ext},
-                no_int = ${ordenData.no_int},
-                estado_id = ${ordenData.estado_id},
-                municipio_id = ${ordenData.municipio_id},
-                ciudad = ${ordenData.ciudad},
-                colonia = ${ordenData.colonia},
-                url_receta = ${ordenData.url_receta},
-                url_solic_analisis = ${ordenData.url_solic_analisis},
-                url_constacia = ${ordenData.url_constacia},
-                url_cert_med = ${ordenData.url_cert_med},
-                url_fact = ${ordenData.url_fact},
-                certificado = ${ordenData.certificado},
-                url_sello = ${ordenData.url_sello},
-                WHERE id_consultorio = ${ordenData.id_orden}`;
+                expediente = '${ordenData.expediente}',
+                descripcion  = '${ordenData.descripcion}',
+                id_servicio = ${ordenData.id_servicio},
+                id_aseguradora = ${ordenData.id_aseguradora},
+                benef_nombre = '${ordenData.benef_nombre}',
+                benef_paterno  = '${ordenData.benef_paterno}',
+                benef_materno  = '${ordenData.benef_materno}',
+                recibe_nombre  = '${ordenData.recibe_nombre}',
+                recibe_materno  = '${ordenData.recibe_materno}',
+                recibe_paterno  = '${ordenData.recibe_paterno}',
+                id_tecnico  = ${ordenData.id_tecnico},
+                calle  = '${ordenData.calle}',
+                num_ext  = '${ordenData.num_ext}',
+                num_int  = '${ordenData.num_int}',
+                col  = '${ordenData.col}',
+                ciudad  = '${ordenData.ciudad}',
+                estado  = '${ordenData.estado}',
+                entre_calle1  = '${ordenData.entre_calle1}',
+                entre_calle2  = '${ordenData.entre_calle2}',
+                referencia  = '${ordenData.referencia}',
+                vehiculo_tipo  = '${ordenData.vehiculo_tipo}',
+                vehiculo_color  = '${ordenData.vehiculo_color}',
+                vehiculo_placa  = '${ordenData.vehiculo_placa}',
+                vehiculo_ubicacion  = '${ordenData.vehiculo_ubicacion}',
+                vehiculo_combustible  = '${ordenData.vehiculo_combustible}',
+                vehiculo_litros = '${ordenData.vehiculo_litros}'
+                WHERE id_orden = ${ordenData.id_orden}`;
         dbAdmin.query(sql, function (error, rows){
             if (error) {
                 console.log(error);
